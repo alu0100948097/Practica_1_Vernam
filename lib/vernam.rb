e1a75558d0fcafd52b84f4c2d98e6747d027c537
Sergio Ferrera de Diego <alu0100948097@ull.edu.es>
2018-02-06 17:17:48 +0000

    Creación de menu

--- lib/vernam.rb
+++ lib/vernam.rb
@@ -1,40 +1,75 @@
 class Vernam
 	
 	attr_writer :mensaje, :clave
+	attr_reader :m_binario, :m_c_binario
 	
 	def initialize
 		@mensaje=""
 		@clave=""
 		@m_binario=""
+		@m_c_binario=[]
 	end
 	
 	def binario
         @m_binario=@mensaje.unpack('B*')
-        puts "Mensaje en binario: #{@m_binario.join}"
-        puts "Longitud del mensaje: #{@m_binario.join.bytesize}"
-		puts
-		
 	end
 	
 	def cifrado
-		m_c_binario=@m_binario[0].unpack('C*').zip(@clave.unpack('C*'))
-		m_c_binario=m_c_binario.map{ |a,b| a^b }
-		puts "Mensaje cifrado en binario: #{m_c_binario.join}"
-		puts "Mensaje cifrado: #{[m_c_binario.join.to_s].pack('B*')}"
+		@m_c_binario=@m_binario[0].unpack('C*').zip(@clave.unpack('C*'))
+		@m_c_binario=@m_c_binario.map{ |a,b| a^b }
 	end
-	
 end
 
 @test=Vernam.new
-puts
-puts "Introduzca el mensaje original:"
-mensaje=gets.chomp
-puts
-@test.mensaje=mensaje
-@test.binario
-puts "Introduza clave:"
-clave=gets.chomp
-puts
-@test.clave=clave
-@test.cifrado
-puts
+
+loop do
+	puts "PRACTICA: CIFRADO DE VERNAM"
+	puts "1: Cifrar"
+	puts "2: Descifrar"
+	puts "3: Salir"
+	print 'Introduzca una opcion: '
+	opcion = gets.chomp
+	case opcion
+		when '1'
+			system "clear"
+			puts
+			print "Introduzca el mensaje original: "
+			mensaje=gets.chomp
+			puts
+			@test.mensaje=mensaje
+			@test.binario
+			puts "Mensaje original en binario: #{@test.m_binario.join}"
+			puts "Longitud del mensaje binario: #{@test.m_binario.join.bytesize}"
+			puts
+			print "Introduza la clave aleatoria: "
+			clave=gets.chomp
+			puts
+			@test.clave=clave
+			@test.cifrado
+			puts "Mensaje cifrado en binario: #{@test.m_c_binario.join}"
+		    puts "Mensaje cifrado: #{[@test.m_c_binario.join.to_s].pack('B*')}"
+			puts
+		when '2'
+			system "clear"
+			puts
+			print "Introduzca el mensaje cifrado: "
+			mensaje=gets.chomp
+			puts
+			@test.mensaje=mensaje
+			@test.binario
+			puts "Mensaje cifrado en binario: #{@test.m_binario.join}"
+			puts "Longitud del mensaje binario: #{@test.m_binario.join.bytesize}"
+			puts
+			print "Introduza la clave aleatoria: "
+			clave=gets.chomp
+			puts
+			@test.clave=clave
+			@test.cifrado
+			puts "Mensaje original en binario: #{@test.m_c_binario.join}"
+		    puts "Mensaje original: #{[@test.m_c_binario.join.to_s].pack('B*')}"
+			puts
+		when '3'
+			system "clear"
+			break
+	end
+end
